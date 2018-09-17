901a2c4c9fe0e4a5c27c5091ff9d1f54571af8da
lukazagar96@gmail.com <Killerman1710>
2018-09-17 14:03:04 +0200

    With Azure ML

---
+++ RUAP-project/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.brojCentara = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.brojIteracija = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.brojCentara)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.brojIteracija)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(9, 12);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(690, 644);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(386, 690);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Učitaj sliku";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(708, 723);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(114, 27);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Grupiraj";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // brojCentara
+            // 
+            this.brojCentara.InterceptArrowKeys = false;
+            this.brojCentara.Location = new System.Drawing.Point(582, 698);
+            this.brojCentara.Margin = new System.Windows.Forms.Padding(2);
+            this.brojCentara.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.brojCentara.Name = "brojCentara";
+            this.brojCentara.Size = new System.Drawing.Size(112, 20);
+            this.brojCentara.TabIndex = 4;
+            this.brojCentara.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.brojCentara.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(584, 671);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 24);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Broj centara";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(735, 12);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(690, 644);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(711, 673);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 24);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Broj iteracija";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // brojIteracija
+            // 
+            this.brojIteracija.Location = new System.Drawing.Point(710, 699);
+            this.brojIteracija.Margin = new System.Windows.Forms.Padding(2);
+            this.brojIteracija.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.brojIteracija.Name = "brojIteracija";
+            this.brojIteracija.Size = new System.Drawing.Size(112, 20);
+            this.brojIteracija.TabIndex = 9;
+            this.brojIteracija.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(828, 671);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(152, 24);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Mjera udaljenosti";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(827, 698);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(148, 21);
+            this.comboBox1.TabIndex = 11;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(205, 690);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(114, 34);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Snimi sliku";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1428, 770);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.brojIteracija);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.brojCentara);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.brojCentara)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.brojIteracija)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown brojCentara;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown brojIteracija;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ RUAP-project/Form1.cs
@@ -0,0 +1,276 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        int imageHeight, imageWidth, rows;
+        string[,] dataset;
+        string filePath = String.Empty;
+        static string[,] datasetTranformed;
+        Bitmap transformedImage;
+
+        public class StringTable
+        {
+            public string[] ColumnNames { get; set; }
+            public string[,] Values { get; set; }
+        }
+
+        public Form1()
+        {
+           
+            InitializeComponent();
+            comboBox1.Items.Add("Euclidean");
+            comboBox1.Items.Add("Cosine"); //Iz nekog razloga se nemože koristiti parametar udaljenosti
+            comboBox1.SelectedIndex = 0; //Odabir početnog elementa(parametra) padajućeg izbornika
+            button3.Visible = false ;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+           
+            OpenFileDialog of = new OpenFileDialog();
+            of.Filter = "Image Files (*.bmp;*.jpg;*.jpeg,*.png)|*.BMP;*.JPG;*.JPEG;*.PNG";
+            if (of.ShowDialog() == DialogResult.OK)
+            {
+                filePath = of.FileName;
+                pictureBox1.ImageLocation = of.FileName;
+
+               
+            }
+
+            Bitmap image = new Bitmap(of.FileName);
+            transformedImage = new Bitmap(image);
+            imageHeight = image.Height;
+            imageWidth = image.Width;
+
+            rows = imageHeight * imageWidth;
+            int row = 0;
+            dataset = new string[rows, 6]; 
+            for (int i = 0; i < imageWidth; i++)
+            {
+                for (int j = 0; j < imageHeight; j++)
+                {
+                    //Priprema podataka
+                    Color pixel = image.GetPixel(i, j);
+                    dataset[row, 0] = i.ToString();
+                    dataset[row, 1] = j.ToString();
+                    dataset[row, 2] = pixel.R.ToString();
+                    dataset[row, 3] = pixel.G.ToString();
+                    dataset[row, 4] = pixel.B.ToString();
+                    dataset[row, 5] = pixel.A.ToString();
+                    row++;
+
+                }
+            }
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Upravljanje greškama
+            if (filePath == String.Empty)
+            {
+                MessageBox.Show("Unesite sliku!", "Upozorenje!",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (brojCentara.Value <2)
+            {
+                MessageBox.Show("Broj centara mora biti veći od 2!", "Upozorenje!",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (brojIteracija.Value < 1)
+            {
+                MessageBox.Show("Broj iteracija mora biti veći od 1!", "Upozorenje!",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (comboBox1.SelectedItem == null)
+            {
+                MessageBox.Show("Odaberite mjeru udaljenosti", "Upozorenje!",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            
+            int numberOfCentroids = Convert.ToInt32(Math.Round(brojCentara.Value, 0));
+            int iterations = Convert.ToInt32(Math.Round(brojIteracija.Value, 0));
+
+            string Metric = comboBox1.SelectedItem.ToString();
+
+            InvokeRequestResponseService(iterations, numberOfCentroids, Metric, dataset, rows).Wait();
+
+            if (datasetTranformed != null)
+            {
+                int x, y, R, G, B, A;
+                for (int i = 0; i < rows; i++)
+                {
+                    //Učitavanje obrađene slike
+                    x = Int32.Parse(dataset[i, 0]);
+                    y = Int32.Parse(dataset[i, 1]);
+                    R = (int)double.Parse(datasetTranformed[i, 0], CultureInfo.InvariantCulture.NumberFormat);
+                    G = (int)double.Parse(datasetTranformed[i, 1], CultureInfo.InvariantCulture.NumberFormat);
+                    B = (int)double.Parse(datasetTranformed[i, 2], CultureInfo.InvariantCulture.NumberFormat);
+                    A = Int32.Parse(dataset[i, 5]);
+                    transformedImage.SetPixel(x, y, Color.FromArgb(A, R, G, B));
+
+                }
+
+                pictureBox3.Image = transformedImage;
+            }
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.ShowDialog();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+        static async Task InvokeRequestResponseService(int iterations, int numberOfCentroids, string Metric, string [,] dataset, int rows)
+        {
+            using (var client = new HttpClient())
+            {
+                var scoreRequest = new
+                {
+
+                    Inputs = new Dictionary<string, StringTable>() {
+                        {
+                            "input1",
+                            new StringTable()
+                            {
+                                ColumnNames = new string[] {"X", "Y", "R", "G", "B", "A"},
+                                Values = dataset //
+                            }
+                        },
+                    },
+                    GlobalParameters = new Dictionary<string, string>() {
+        { "Iterations", iterations.ToString() },
+        { "Number of Centroids", numberOfCentroids.ToString() },
+        { "Metric", Metric },
+}
+                };
+                const string apiKey = "5Klufp2YzyjtFcjsuHzjQV1MN1MgHQvnJ47wBrrT9PHhYh9HyKminKPgeDX0LqgRpEun7iAYImMhFBfH7Rk4dA=="; //API ključ
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
+
+                client.BaseAddress = new Uri("https://ussouthcentral.services.azureml.net/workspaces/7b7d186476b445d6b6f680c5686dbc81/services/36d41008942644b5a6b0cbd7e112d6a3/execute?api-version=2.0&details=true");
+
+                HttpResponseMessage response = await client.PostAsJsonAsync("", scoreRequest).ConfigureAwait(false);
+
+                 if (response.IsSuccessStatusCode)
+                {
+                    string[] resultArray;
+                    datasetTranformed = new string[rows,3];
+                    int j = 0, x= 0, counter = 0;
+                    Boolean readyToWrite = false;
+                    string RGBstring, partString = String.Empty;
+                    string result = await response.Content.ReadAsStringAsync(); //Odgovor web servisa
+                    resultArray = result.Split(':');//Parsiranje odgovora web servisa
+                    RGBstring = resultArray[7].Replace("}", "");
+                    for (int i = 1; i < RGBstring.Length - 1; i++)
+                    {
+                        if (RGBstring[i] == '[')
+                        {
+                            while (RGBstring[i] != ']')
+                            {
+                                if (RGBstring[i] == '"')
+                                {
+                                    i++;
+                                    counter++;
+                                    if (counter % 2 == 0)
+                                    {
+                                        readyToWrite = true;
+                                    }
+                                }
+                                if (counter % 2 != 0)
+                                {
+                                    partString = partString + RGBstring[i];
+                                }
+                                if(readyToWrite)
+                                {
+                                    datasetTranformed[x,j] = partString; //Spremanje vrijednosti u 2D polje
+                                    j++;
+                                    partString = "";
+                                    readyToWrite = false;
+                                }
+
+                                if (counter == 6)
+                                {
+                                    j = 0;
+                                    break;
+                                }
+
+                                i++;
+
+                            }
+                            x++;
+                            counter = 0;
+                                
+                        }
+                    } 
+
+                }
+                else
+                {
+                    Console.WriteLine(string.Format("The request failed with status code: {0}", response.StatusCode));
+
+                    // Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
+                    Console.WriteLine(response.Headers.ToString());
+
+                    string responseContent = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(responseContent);
+                }
+            }
+        }
+    }
+}
